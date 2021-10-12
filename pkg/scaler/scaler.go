c5049fab4a2678f7e6800c4979eef251869a6d06
Tobias Germer <tobias.germer@tui.com>
2021-10-12 12:30:49 +0200

    refactor logging

--- pkg/scaler/scaler.go
+++ pkg/scaler/scaler.go
@@ -64,7 +64,7 @@ func (s *Scaler) scaleTargets(ctx context.Context) {
 	for _, target := range s.ScalingTargets {
 		err := s.scaleTarget(ctx, target)
 		if err != nil {
-			log.Warn(err)
+			log.WithField("tableName", target.TableName).Warnf("skipping scaling action: %s", err)
 		}
 	}
 }
@@ -72,66 +72,77 @@ func (s *Scaler) scaleTargets(ctx context.Context) {
 func (s *Scaler) scaleTarget(ctx context.Context, target TableScalingConfiguration) error {
 	table, err := s.ddbClient.DescribeTable(ctx, &dynamodb.DescribeTableInput{TableName: aws.String(target.TableName)})
 	if err != nil {
-		return fmt.Errorf("skipping scaling action: failed to get table status for %s: %w", target.TableName, err)
+		return fmt.Errorf("failed to get table status: %w", err)
 	}
 
 	if table.Table.TableStatus != "ACTIVE" {
-		return fmt.Errorf("skipping scaling action: table %s is currently in status: %s", target.TableName, table.Table.TableStatus)
+		return fmt.Errorf("table is currently in status: %s", table.Table.TableStatus)
 	}
 
 	tableCap, err := s.getTableCapacity(ctx, target.TableName)
 	if err != nil {
-		return fmt.Errorf("failed to get table capacity for %s: %w", target.TableName, err)
+		return fmt.Errorf("failed to get table capacity: %w", err)
 	}
 
 	tableCap.readCapacity.provisioned = table.Table.ProvisionedThroughput.ReadCapacityUnits
 	tableCap.writeCapacity.provisioned = table.Table.ProvisionedThroughput.WriteCapacityUnits
 
 	if !tableCap.isSafe() {
-		return fmt.Errorf("table capacity is not safe, skipping table %s: %+v", target.TableName, *tableCap)
+		return fmt.Errorf("table capacity is not safe: %+v", *tableCap)
 	}
 
-	var newReadCap, newWriteCap int64
-
-	if tableCap.readCapacity.throttles > 0 && tableCap.readCapacity.provisioned != nil {
-		newReadCap, err = makeThrottlingScalingDecision(tableCap.readCapacity, target)
-		if err != nil {
-			return fmt.Errorf("skipping scaling action: failed to make read throttle scaling decision for %s: %w", target.TableName, err)
-		}
-	}
-
-	if tableCap.writeCapacity.throttles > 0 && tableCap.writeCapacity.provisioned != nil {
-		newWriteCap, err = makeThrottlingScalingDecision(tableCap.writeCapacity, target)
-		if err != nil {
-			return fmt.Errorf("skipping scaling action: failed to make write throttle scaling decision for %s: %w", target.TableName, err)
-		}
-	}
-
-	if newReadCap == 0 {
-		newReadCap, err = makeLowConsumptionScalingDecision(tableCap.readCapacity, target)
-		if err != nil {
-			return fmt.Errorf("skipping scaling action: failed to make read low consumption scaling decision for %s: %w", target.TableName, err)
-		}
-	}
-
-	if newWriteCap == 0 {
-		newWriteCap, err = makeLowConsumptionScalingDecision(tableCap.writeCapacity, target)
-		if err != nil {
-			return fmt.Errorf("skipping scaling action: failed to make write low consumption scaling decision for %s: %w", target.TableName, err)
-		}
+	newReadCap, newWriteCap, err := s.calculateNewCapacity(tableCap, target)
+	if err != nil {
+		return fmt.Errorf("failed to calculate new capacity: %w", err)
 	}
 
 	if (newReadCap >= 1 || newWriteCap >= 1) &&
 		(newReadCap != *table.Table.ProvisionedThroughput.ReadCapacityUnits || newWriteCap != *table.Table.ProvisionedThroughput.WriteCapacityUnits) {
 		err = s.updateTableCapacity(ctx, table.Table, newReadCap, newWriteCap)
 		if err != nil {
-			return fmt.Errorf("failed to scale table %s: %w", target.TableName, err)
+			return fmt.Errorf("failed to scale table: %w", err)
 		}
 	}
 
-	log.Debugf("no scaling necessary for %s", target.TableName)
+	log.WithField("tableName", target.TableName).Debug("no scaling necessary")
 
 	return nil
+}
+
+func (s *Scaler) calculateNewCapacity(tableCap *tableCapacity, target TableScalingConfiguration) (int64, int64, error) {
+	var newReadCap, newWriteCap int64
+
+	var err error
+
+	if tableCap.readCapacity.throttles > 0 && tableCap.readCapacity.provisioned != nil {
+		newReadCap, err = makeThrottlingScalingDecision(tableCap.readCapacity, target)
+		if err != nil {
+			return 0, 0, fmt.Errorf("failed to make read throttle scaling decision: %w", err)
+		}
+	}
+
+	if tableCap.writeCapacity.throttles > 0 && tableCap.writeCapacity.provisioned != nil {
+		newWriteCap, err = makeThrottlingScalingDecision(tableCap.writeCapacity, target)
+		if err != nil {
+			return 0, 0, fmt.Errorf("failed to make write throttle scaling decision: %w", err)
+		}
+	}
+
+	if newReadCap == 0 {
+		newReadCap, err = makeLowConsumptionScalingDecision(tableCap.readCapacity, target)
+		if err != nil {
+			return 0, 0, fmt.Errorf("failed to make read low consumption scaling decision: %w", err)
+		}
+	}
+
+	if newWriteCap == 0 {
+		newWriteCap, err = makeLowConsumptionScalingDecision(tableCap.writeCapacity, target)
+		if err != nil {
+			return 0, 0, fmt.Errorf("failed to make write low consumption scaling decision: %w", err)
+		}
+	}
+
+	return newReadCap, newWriteCap, nil
 }
 
 func (s *Scaler) updateTableCapacity(ctx context.Context, table *dynamodbTypes.TableDescription, newReadCap int64, newWriteCap int64) error {
@@ -153,10 +164,10 @@ func (s *Scaler) updateTableCapacity(ctx context.Context, table *dynamodbTypes.T
 		ProvisionedThroughput: &provisionThroughput,
 	})
 	if err != nil {
-		return fmt.Errorf("failed to update table: %s", err)
+		return fmt.Errorf("failed to update table: %w", err)
 	}
 
-	log.Infof("updated table capacity for %s: read=%d,write=%d", *table.TableName, newReadCap, newWriteCap)
+	log.WithField("tableName", *table.TableName).Infof("updated table capacity: read=%d,write=%d", newReadCap, newWriteCap)
 
 	return nil
 }
